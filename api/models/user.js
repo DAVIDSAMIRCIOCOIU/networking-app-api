b6b168c447eb823c79864bdcadb5676cb6f124a0
DAVIDCIOCOIU95 <david.samir.ciocoiu@gmail.com>
2020-07-17 15:31:32 +0200

    user required only email and pw

--- api/models/user.js
+++ api/models/user.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    name: {type: String, required: true},
-    surname: {type: String, required: true},
+    name: {type: String},
+    surname: {type: String},
     email: {type: String, required: true, unique: true, match: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/},
     password: {type: String, required: true},
     role: {type: String, enum: ['admin', 'customer', 'networker'], default: 'networker', immutable: true},
